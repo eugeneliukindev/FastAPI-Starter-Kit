7fbe058fd6c40ff6f36f02b1ed2c9a42fbd6df4a
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-19 14:35:22 +0300

    change status to from int to status code from fastapi

--- fastapi-app/exceptions/users.py
+++ fastapi-app/exceptions/users.py
@@ -1,9 +1,11 @@
-from fastapi.exceptions import HTTPException
+from fastapi import HTTPException, status
 
 
 def raise_user_already_exists_exp() -> HTTPException:
-    raise HTTPException(status_code=409, detail="User already exists!")
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT, detail="User already exists!"
+    )
 
 
 def raise_user_not_found_exp() -> HTTPException:
-    raise HTTPException(status_code=404, detail="User not found!")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found!")
