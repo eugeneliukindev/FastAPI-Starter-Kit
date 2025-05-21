031862a8cf0964368409d682bab5218b306dba85
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-21 19:44:53 +0300

    add exceptions

---
+++ app/exceptions/auth.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException, status
+
+incorrect_username_or_password_exp = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Incorrect username or password",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+unverified_credentials_exc = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+invalid_token_type_exc = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid token type",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+token_expired_exc = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token has expired",
+    headers={"WWW-Authenticate": "Bearer"},
+)
