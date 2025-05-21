134d6d3a537c3c534f46d05d94067c1ee952e241
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-21 19:41:30 +0300

    Add types.py

--- app/utils/types.py
+++ app/utils/types.py
@@ -1,11 +1,5 @@
-from typing import Annotated
+from typing import Literal
 
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.core.db_manager import db_manager
-
-SessionDep = Annotated[
-    AsyncSession,
-    Depends(db_manager.session_getter),
-]
+type AlgorithmType = Literal["RS256"]
+type TokenType = Literal["access", "refresh"]
+type AuthenticationSchemeType = Literal["Bearer"]
