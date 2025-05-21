e6c2fc3ff75330384a4ab544cac060f4f2e952db
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-21 20:00:20 +0300

    Add router auth.py

---
+++ app/api/v1/endpoints/auth.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
+
+from app.auth.dependencies import (
+    authenticate_user,
+    get_auth_user_from_access_token,
+    get_auth_user_from_refresh_token,
+)
+from app.auth.token import (
+    create_access_token,
+    create_refresh_token,
+)
+from app.core.db_manager import SessionDep
+from app.core.schemas import UserS
+from app.core.schemas.token import TokenResponseS
+from app.utils.constants import (
+    AUTHENTICATION_SCHEME_TYPE,
+)
+
+http_bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(prefix="/auth", tags=["Auth"], dependencies=[Depends(http_bearer)])
+
+
+@router.post(
+    "/token",
+    response_model=TokenResponseS,
+)
+async def token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep,
+) -> Any:
+    user = await authenticate_user(session, form_data.username, form_data.password)
+    access_token = create_access_token(user=user)
+    refresh_token = create_refresh_token(user=user)
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "authentication_scheme": AUTHENTICATION_SCHEME_TYPE,
+    }
+
+
+@router.get(
+    "/refresh",
+    response_model=TokenResponseS,
+    response_model_exclude_none=True,
+)
+async def refresh(
+    user: Annotated[UserS, Depends(get_auth_user_from_refresh_token)],
+) -> Any:
+    access_token = create_access_token(user=user)
+    return {
+        "access_token": access_token,
+        "authentication_scheme": AUTHENTICATION_SCHEME_TYPE,
+    }
+
+
+@router.get("/me", response_model=UserS)
+async def get_me(
+    user: Annotated[UserS, Depends(get_auth_user_from_access_token)],
+) -> Any:
+    return user
--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
+from app.api.v1.endpoints.auth import router as auth_router
 from app.api.v1.endpoints.users import router as user_router
 
 api_v1_router = APIRouter(prefix="/v1")
 
 router_list = [
     user_router,
+    auth_router,
 ]
 
 for r in router_list:
