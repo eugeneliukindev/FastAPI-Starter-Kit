c559db5524df1f73fa63ba498b58af9d4c57931e
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-21 19:44:35 +0300

    Add hashed password to user model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -8,3 +8,4 @@ from .mixins import IntIdPkMixin
 class UserOrm(Base, IntIdPkMixin):
     username: Mapped[str] = mapped_column(String(30), unique=True)
     email: Mapped[str] = mapped_column(String(50), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(100), unique=True)
