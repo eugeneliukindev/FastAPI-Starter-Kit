2134273fc605f3c8e60fa0fdc21d19fecd0f70ec
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-21 19:59:35 +0300

    add auth dependencies

---
+++ app/auth/dependencies.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.password import verify_password
+from app.auth.token import decode_token, validate_token
+from app.core.db_manager import SessionDep
+from app.core.schemas import UserS
+from app.exceptions.auth import (
+    incorrect_username_or_password_exp,
+    invalid_token_type_exc,
+)
+from app.repository import UserRepository
+from app.utils.constants import ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYPE
+from app.utils.types import TokenType
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/token")
+
+
+async def authenticate_user(
+    session: AsyncSession, username: str, password: str
+) -> UserS:
+    user = await UserRepository.get_by_username(session=session, username=username)
+    if user is None or not verify_password(password, user.hashed_password):
+        raise incorrect_username_or_password_exp
+    return UserS.model_validate(user, from_attributes=True)
+
+
+async def get_auth_user(
+    session: AsyncSession,
+    token: str,
+    expected_token_type: TokenType,
+) -> UserS:
+    payload = decode_token(token=token)
+    validate_token(payload=payload, expected_token_type=expected_token_type)
+    user = await UserRepository.get_by_username(session=session, username=payload.sub)
+    if user is None:
+        raise invalid_token_type_exc
+    return UserS.model_validate(user, from_attributes=True)
+
+
+async def get_auth_user_from_access_token(
+    session: SessionDep,
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> UserS:
+    return await get_auth_user(
+        session=session, token=token, expected_token_type=ACCESS_TOKEN_TYPE
+    )
+
+
+async def get_auth_user_from_refresh_token(
+    session: SessionDep,
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> UserS:
+    return await get_auth_user(
+        session=session, token=token, expected_token_type=REFRESH_TOKEN_TYPE
+    )
