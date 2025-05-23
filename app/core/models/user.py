b5f769b36b713dd5a6adb477cebd65e318bbdbeb
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-23 20:51:16 +0300

    Update User model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,11 +1,15 @@
-from sqlalchemy import String
+from sqlalchemy import Boolean, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-from .mixins import IntIdPkMixin
+from .mixins import IntIdPkMixin, TimestampMixin
 
 
-class UserOrm(Base, IntIdPkMixin):
-    username: Mapped[str] = mapped_column(String(30), unique=True)
+class UserOrm(Base, IntIdPkMixin, TimestampMixin):
+    username: Mapped[str] = mapped_column(String(50), unique=True)
     email: Mapped[str] = mapped_column(String(50), unique=True)
     hashed_password: Mapped[str] = mapped_column(String(100), unique=True)
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
