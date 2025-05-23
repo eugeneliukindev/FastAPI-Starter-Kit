131f6082c039f8f13c8cf1aa4c5bc685fae423ec
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-23 20:51:53 +0300

    Fix annotations and optimize imports

--- app/core/schemas/user.py
+++ app/core/schemas/user.py
@@ -1,31 +1,74 @@
-from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import final
+
+from pydantic import BaseModel, ConfigDict, EmailStr, PositiveInt
 
 
-class UserBaseOptionalS(BaseModel):
-    username: str | None = None
-    email: EmailStr | None = None
-
-
-class UserBaseS(UserBaseOptionalS):
-    username: str
-    email: EmailStr
-
-
-class UserS(UserBaseS):
-    id: int
+class UserBaseS(BaseModel):
+    pass
 
 
 class UserCreateS(UserBaseS):
+    username: str
     password: str
+    email: EmailStr
 
 
-class UserCreateDatabaseS(UserBaseS):
+class UserCreateDbS(UserBaseS):
+    username: str
     hashed_password: str
+    email: EmailStr
+
+
+@final
+class UserS(UserBaseS):  # UserOrm
+    model_config = ConfigDict(from_attributes=True)
+
+    id: PositiveInt
+    username: str
+    email: EmailStr
+    hashed_password: str  # !
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
+    created_at: datetime
+    updated_at: datetime
+    last_login_at: datetime | None
+
+
+class UserReadS(UserBaseS):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: PositiveInt
+    username: str
+    email: EmailStr
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
+    created_at: datetime
+    updated_at: datetime
+    last_login_at: datetime | None
+
+
+class UserUpdateS(UserBaseS):
+    username: str
+    email: EmailStr
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
 
 
 class UserPutS(UserBaseS):
-    pass
+    username: str
+    email: EmailStr
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
 
 
-class UserPatchS(UserBaseOptionalS):
-    pass
+class UserPatchS(UserBaseS):
+    username: str | None = None
+    email: EmailStr | None = None
+    is_active: bool | None = None
+    is_superuser: bool | None = None
+    is_verified: bool | None = None
