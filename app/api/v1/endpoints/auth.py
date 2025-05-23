37c3f8acb83c6b04eb7d70e39ac63dbac29d46a0
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-23 20:57:09 +0300

    Optimize endpoints logic

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Annotated, Any
+from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
@@ -9,13 +9,17 @@ from app.auth.dependencies import (
     authenticate_user,
     get_user_from_refresh_token,
 )
+from app.auth.password import get_password_hash
 from app.auth.token import (
     create_access_token,
     create_refresh_token,
 )
 from app.core.db_manager import SessionDep
-from app.core.schemas import UserS
-from app.core.schemas.token import TokenResponseS
+from app.core.schemas import UserCreateS
+from app.core.schemas.token import TokenReadS
+from app.core.schemas.user import UserCreateDbS, UserReadS
+from app.exceptions.auth import already_exists_user_exc
+from app.repository import UserRepository
 from app.utils.constants import (
     AUTHENTICATION_SCHEME_TYPE,
 )
@@ -25,34 +29,53 @@ http_bearer = HTTPBearer(auto_error=False)
 router = APIRouter(prefix="/auth", tags=["Auth"], dependencies=[Depends(http_bearer)])
 
 
-@router.post(
-    "/login",
-    response_model=TokenResponseS,
-)
+@router.post("/login")
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     session: SessionDep,
-) -> Any:
+) -> TokenReadS:
     user = await authenticate_user(session, form_data.username, form_data.password)
-    access_token = create_access_token(user=user)
-    refresh_token = create_refresh_token(user=user)
-    return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
-        "authentication_scheme": AUTHENTICATION_SCHEME_TYPE,
-    }
+    access_token = create_access_token(user_read_s=user)
+    refresh_token = create_refresh_token(user_read_s=user)
+    return TokenReadS(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        authentication_scheme=AUTHENTICATION_SCHEME_TYPE,
+    )
 
 
-@router.post(
-    "/refresh",
-    response_model=TokenResponseS,
-    response_model_exclude_none=True,
-)
+@router.post("/refresh", response_model_exclude_none=True)
 async def refresh(
-    user: Annotated[UserS, Depends(get_user_from_refresh_token)],
-) -> Any:
-    access_token = create_access_token(user=user)
-    return {
-        "access_token": access_token,
-        "authentication_scheme": AUTHENTICATION_SCHEME_TYPE,
-    }
+    user: Annotated[UserReadS, Depends(get_user_from_refresh_token)],
+) -> TokenReadS:
+    access_token = create_access_token(user_read_s=user)
+    return TokenReadS(
+        access_token=access_token,
+        authentication_scheme=AUTHENTICATION_SCHEME_TYPE,
+    )
+
+
+@router.post("/register")
+async def register(user_create_s: UserCreateS, session: SessionDep) -> TokenReadS:
+    existing_user = await UserRepository.check_already_exists(
+        session=session,
+        username=user_create_s.username,
+        email=str(user_create_s.email),
+    )
+    if existing_user:
+        raise already_exists_user_exc
+    hashed_password = get_password_hash(password=user_create_s.password)
+    user_create_db_s = UserCreateDbS(
+        username=user_create_s.username,
+        hashed_password=hashed_password,
+        email=user_create_s.email,
+    )
+    user = await UserRepository.create(session=session, user_create_db_s=user_create_db_s)
+    user_read_s = UserReadS.model_validate(user)
+    access_token = create_access_token(user_read_s=user_read_s)
+    refresh_token = create_refresh_token(user_read_s=user_read_s)
+    return TokenReadS(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        authentication_scheme=AUTHENTICATION_SCHEME_TYPE,
+    )
--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -10,17 +10,18 @@ from fastapi import (
 from app.auth.dependencies import get_user_from_access_token
 from app.auth.password import get_password_hash
 from app.core.db_manager import SessionDep
-from app.core.schemas import UserCreateS, UserPutS, UserS
-from app.core.schemas.user import UserCreateDatabaseS, UserPatchS
-from app.exceptions.users import user_already_exists_exp, user_not_found_exp
+from app.core.schemas import UserCreateS, UserPutS
+from app.core.schemas.user import UserCreateDbS, UserPatchS, UserReadS
+from app.exceptions.users import user_not_found_exp
 from app.repository import UserRepository
 
 if TYPE_CHECKING:
     from typing import Any
+
 router = APIRouter(tags=["Users"], prefix="/users")
 
 
-@router.get("", response_model=list[UserS])
+@router.get("", response_model=list[UserReadS])
 async def get_all_users(
     session: SessionDep,
 ) -> Any:
@@ -28,34 +29,30 @@ async def get_all_users(
     return users
 
 
-@router.post("", response_model=UserS)
+@router.post("", response_model=UserReadS)
 async def create_user(
     session: SessionDep,
     user_create: UserCreateS,
 ) -> Any:
     hashed_password = get_password_hash(password=user_create.password)
-    user_create_db = UserCreateDatabaseS(
-        **user_create.model_dump(exclude={"password"}),
+    user_create_db_s = UserCreateDbS(
+        username=user_create.username,
         hashed_password=hashed_password,
+        email=user_create.email,
     )
     user = await UserRepository.create(
         session=session,
-        user_create_db=user_create_db,
+        user_create_db_s=user_create_db_s,
     )
-    if user is None:
-        raise user_already_exists_exp
-
     return user
 
 
-@router.get("/me", response_model=UserS)
-async def get_me(
-    user: Annotated[UserS, Depends(get_user_from_access_token)],
-) -> Any:
+@router.get("/me")
+async def get_me(user: Annotated[UserReadS, Depends(get_user_from_access_token)]) -> Any:
     return user
 
 
-@router.get("/{user_id}", response_model=UserS)
+@router.get("/{user_id}", response_model=UserReadS)
 async def get_user(
     user_id: int,
     session: SessionDep,
@@ -66,37 +63,31 @@ async def get_user(
     return user
 
 
-@router.put("/{user_id}", response_model=UserS)
+@router.put("/{user_id}", response_model=UserReadS)
 async def update_user_partial(
     session: SessionDep,
     user_id: int,
     user_put: UserPutS,
 ) -> Any:
-    updated_user = await UserRepository.update(
-        session=session, user_id=user_id, user_update=user_put
-    )
+    updated_user = await UserRepository.update(session=session, user_id=user_id, user_update=user_put)
     if updated_user is None:
         raise user_not_found_exp
     return updated_user
 
 
-@router.patch("/{user_id}", response_model=UserS)
+@router.patch("/{user_id}", response_model=UserReadS)
 async def update_user(
     session: SessionDep,
     user_id: int,
     user_patch: UserPatchS,
 ) -> Any:
-    updated_user = await UserRepository.update(
-        session=session,
-        user_id=user_id,
-        user_update=user_patch,
-    )
+    updated_user = await UserRepository.update(session=session, user_id=user_id, user_update=user_patch)
     if updated_user is None:
         raise user_not_found_exp
     return updated_user
 
 
-@router.delete("/{user_id}", response_model=UserS)
+@router.delete("/{user_id}", response_model=UserReadS)
 async def delete_user(
     session: SessionDep,
     user_id: int,
