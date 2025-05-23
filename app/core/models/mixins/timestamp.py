322c49017224083ff29d967df08ea6e22a16dddc
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-23 20:51:02 +0300

    Add timestamp mixin

--- app/core/models/mixins/timestamp.py
+++ app/core/models/mixins/timestamp.py
@@ -5,10 +5,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.now(UTC), nullable=False
-    )
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.now(UTC), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC), nullable=False
+        DateTime(timezone=True), default=datetime.now(UTC), onupdate=datetime.now(UTC), nullable=False
     )
-    last_login_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+    last_login_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
