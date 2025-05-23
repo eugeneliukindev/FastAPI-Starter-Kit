0401a67c7cde0001a519ffb66d924294e79c4d31
Eugene Liukin <eugeneliukin.dev@gmail.com>
2025-05-23 20:52:01 +0300

    Change var name

--- app/core/schemas/token.py
+++ app/core/schemas/token.py
@@ -4,7 +4,7 @@ from app.utils.constants import AUTHENTICATION_SCHEME_TYPE
 from app.utils.types import AuthenticationSchemeType, TokenType
 
 
-class TokenResponseS(BaseModel):
+class TokenReadS(BaseModel):
     access_token: str
     refresh_token: str | None = None
     authentication_scheme: AuthenticationSchemeType = AUTHENTICATION_SCHEME_TYPE
